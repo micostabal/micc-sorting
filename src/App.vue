ae2ffa2ed66e45791beaf4b32223cc7592dd1198
Martín Costabal <13747700+micostabal@users.noreply.github.com>
2023-06-17 16:12:41 -0400

    First commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+        <a href="https://flowbite.com" class="flex items-center">
+            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+        </a>
+        <div class="flex items-center">
+            <a href="#" class="text-sm mr-6 text-blue-600 dark:text-blue-500 hover:underline">Sign Up</a>
+            <a href="#" class="text-sm mr-6 text-blue-600 dark:text-blue-500 hover:underline">Sign In</a>
+        </div>
+    </div>
+  </nav>
+  <nav class="bg-gray-50 dark:bg-gray-700">
+      <div class="max-w-screen-xl px-4 py-3 mx-auto">
+          <div class="flex items-center">
+              <ul class="flex flex-row font-medium mt-0 mr-6 space-x-8 text-sm">
+                  <li>
+                      <a href="#" class="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
+                  </li>
+                  <li>
+                      <a href="#" class="text-gray-900 dark:text-white hover:underline">Company</a>
+                  </li>
+                  <li>
+                      <a href="#" class="text-gray-900 dark:text-white hover:underline">Team</a>
+                  </li>
+                  <li>
+                      <a href="#" class="text-gray-900 dark:text-white hover:underline">Features</a>
+                  </li>
+              </ul>
+          </div>
+      </div>
+  </nav>
+  
+  <div class="grid grid-cols-2 gap-4 place-content-center h-48 ">
+    <div>01</div>
+    <div>02</div>
+    <div>03</div>
+    <div>04</div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
